69dee55bcc85dcb2270c7cf4e788123e415916b3
Mahima2423 <162468422+Mahima2423@users.noreply.github.com>
2024-04-14 02:37:06 +0000

    DataTyes revisited

---
+++ datatypes_revisited.js
@@ -0,0 +1,50 @@
+// Primitive => 7
+
+// 7 types : Number, string, Boolean, null, undefined, symbol, BigInt
+
+
+var score = 100       //number
+var score = 100.3     //number
+
+const myString = "mahima"  //string
+const isLogged = false      // boolean
+const temp = null       //null
+let name ;        //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);     // false
+
+const bigNum = 34527865484564567845456789n   //bigint
+console.log(typeof bigNum)
+
+// reference (Non primitive)
+
+// Array, Object, Functions
+
+let heroes = ["shaktiman", "superman" , "spiderman"];
+console.log(typeof heroes);
+
+let obj = {
+    name: "mahima",
+    age: 24,
+}
+
+const myfun = function() {
+    console.log("hello world");
+}
+
+/*    Datatype          Return Type
+    1. number           number
+    2. string           string
+    3. bigint           bigint
+    4. null             object**
+    5. undefined        undefined
+    6. symbol           symbol
+    7. boolean          boolean
+    8. array            object**
+    9. object           object
+    10. function        function**
+
+*/
